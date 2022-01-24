4971dde22871ebafa41065bfe7da48591c16d67b
ryoheinamiki515 <46077588+ryoheinamiki515@users.noreply.github.com>
2022-01-23 18:35:15 -0600

    Add files via upload

--- solver.py
+++ solver.py
@@ -1,20 +1,18 @@
-from typing import List, Optional
-from itertools import product
 from collections import Counter
-from unittest import mock
+from itertools import product
+from typing import List, Optional
 
+import matplotlib.pyplot as plt
 
-COMBINATIONS = ["".join(a) for a in product(['_', 'y', 'g'], repeat=5)]
 
 def get_words_from_file(filename) -> List[str]:
     with open(filename) as f:
         return [x.strip() for x in f.readlines()]
 
 class WordleSolver:
-    def __init__(self, all_words: List[str], answer: Optional[str] = None, first_guess: Optional[str] = None, num_chars: int = 5) -> None:
-        self.answer = answer
+    def __init__(self, all_words: List[str], first_guess: Optional[str] = None, num_chars: int = 5) -> None:
         self.all_words = all_words
-
+        self.cache = None
         if first_guess:
             self.first_guess = first_guess.lower()
             assert len(first_guess) == num_chars, "first_guess must be the same length as num_chars (default 4)"
@@ -61,26 +59,52 @@ class WordleSolver:
     def filter_results(self, guess, result, prev_valid_words):
         return [word for word in prev_valid_words if self.evaluate_guess(guess, word) == result]
 
-    def solve(self):
+    def cache_results(self, first_guess):
+        cached = dict()
+        combinations = ["".join(a) for a in product(['_', 'y', 'g'], repeat=len(first_guess))]
+        for result in combinations:
+            cached[result] = self.filter_results(first_guess, result, self.all_words)
+        self.cache = cached
+
+    def solve(self, answer: Optional[str] = None) -> int:
+        count = 0
         valid_words = self.all_words
         guess = self.first_guess if self.first_guess else self.get_best_word(self.all_words)
         while True:
-            print(guess)
-            print(valid_words)
-            if self.answer:
-                guess_result = self.evaluate_guess(guess, self.answer)
+            count += 1
+            if answer:
+                guess_result = self.evaluate_guess(guess, answer)
             else:
+                print(guess)
                 guess_result = input("What was the result of the guess?\n")
-            # filter valid words based on guess_result
-            valid_words = self.filter_results(guess, guess_result, valid_words)
+            if guess_result == "ggggg":
+                break
+            if guess == self.first_guess and self.cache:
+                valid_words = self.cache[guess_result]
+            else:
+                # filter valid words based on guess_result
+                valid_words = self.filter_results(guess, guess_result, valid_words)
             if len(valid_words) == 1:
                 guess = valid_words[0]
             else:
                 guess = self.get_best_word(valid_words)
+        return count
         
 
 
 if __name__ == "__main__":
     all_words = get_words_from_file("wordle-words.txt")
     solver = WordleSolver(all_words, first_guess="raise")
-    print(solver.solve())
+    solver.solve()
+    # solver.cache_results("raise")
+    # guessCounts = []
+    # for word in all_words:
+    #     guessCounts.append(solver.solve(word))
+    # guessCounter = Counter(guessCounts)
+    # x = [num for num in guessCounter]
+    # y = [guessCounter[num] for num in guessCounter]
+    # plt.bar(x, y)
+    # plt.xlabel("Number of guesses")
+    # plt.ylabel("Occurrences")
+    # plt.show()
+    # print(sum(guessCounts)/len(guessCounts))
